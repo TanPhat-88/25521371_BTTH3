8bfce8ecd8166222c4ea1518ed5418169a9d22d1
TanPhat-88 <dieulinh0887@gmail.com>
2026-04-23 21:02:29 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "cListCandidate.h"
+
+int main() {
+    cListCandidate list;
+    
+    list.NhapDS();
+    list.XuatTren15();
+    list.ThiSinhDiemCaoNhat();
+    list.SapXepGiam();
+    return 0;
+}
